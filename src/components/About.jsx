26dfadc3d1f76748062c8699123f8a7fae43fe72
kasheeumar <kshfkhl@gmail.com>
2023-05-02 11:03:49 +0500

    about section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,31 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px]"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 m-h-[280px] flex justify-evenly items-center flex-col cursor-pointer"
+        >
+          <img src={icon} alt="icon" className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
 const About = () => {
   return (
     <>
@@ -17,7 +42,19 @@ const About = () => {
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-      ></motion.p>
+      >
+        I'm skilled sofware develpor with experince in Typescript and javascrip,
+        and expertise in frameworks like React, NodeJs and ThreeJs. I'm a back
+        learner and collaborate closely with clients to create efficient,
+        scalable and user-friendly solutions that solve real-world problems.
+        Let's work togather to bring your ideas to life !
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
